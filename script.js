ddda386fc608bb84dc934d547a8c57ca1b61b44d
ongaeshi <ongaeshi0621@gmail.com>
2024-04-16 02:05:33 +0900

    feat: Improve

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function checkPassword() {
     var result = document.getElementById('result');
 
     if (parentPassword === childPassword) {
-        result.innerHTML = '<p>正解！</p><img src="cute-image.jpg" alt="かわいい絵">';
+        result.innerHTML = '<p>正解！</p><img src="cute-image.webp" alt="かわいい絵" width="512px">';
     } else {
         result.textContent = '不一致です。もう一度試してください。';
     }
